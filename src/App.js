4e4301607d6a9bfc281cfd6324d579fdc2bbb6d3
libevm <libevm@proton.me>
2023-10-31 17:25:59 +1100

    cal mobile

--- src/App.js
+++ src/App.js
@@ -1,110 +1,57 @@
-import jsonp from "jsonp";
-import { useState } from "react";
 import logo from "./logo.png";
 import {
-  Button,
-  Modal,
-  Box,
   Grid,
   Link,
   Typography,
-  TextField,
+  Container
 } from "@mui/material";
 
-const modalStyle = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: 400,
-  bgcolor: "background.paper",
-  border: "2px solid #000",
-  boxShadow: 24,
-  p: 4,
-};
+
 
 function App() {
-  const [modalOpen, setModalOpen] = useState(false);
-  const [email, setEmail] = useState("");
-
   return (
-    <>
-      <div>
-        <Modal
-          open={modalOpen}
-          onClose={() => setModalOpen(false)}
-          aria-labelledby="modal-modal-title"
-          aria-describedby="modal-modal-description"
-        >
-          <Box sx={modalStyle}>
-            <Typography id="modal-modal-title" variant="h6" component="h2">
-              Subscribe to Ethereum Melbourne Newsletter
-            </Typography>
-            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-              <TextField
-                id="outlined-basic"
-                label="Email"
-                variant="outlined"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                fullWidth
-              />
-            </Typography>
-            <br />
-            <Button
-              onClick={(e) => {
-                e.preventDefault();
-                const url =
-                  "https://ethmelbourne.us9.list-manage.com/subscribe/post-json?u=b2e8f929d8b60036db4b12250&amp;id=3dd138967a&amp;f_id=00c43ce1f0";
-                jsonp(`${url}&EMAIL=${email}`, { param: "c" }, (_, data) => {
-                  // do something with response
-                  // alert(msg);
-                  alert("Subscription successful");
-                  setEmail("");
-                });
-                setModalOpen(false);
-              }}
-              fullWidth
-              variant="contained"
-            >
-              Subscribe
-            </Button>
-          </Box>
-        </Modal>
-      </div>
-      <Grid
-        container
-        spacing={0}
-        direction="column"
-        alignItems="center"
-        justifyContent="center"
-        style={{ minHeight: "100vh", textAlign: "center" }}
-      >
-        <Grid item xs={3}>
-          <img src={logo} alt="logo" width={128} height={128} />
-          <Typography variant="h4">Ethereum Melbourne</Typography>
-          <Typography variant="subtitle1">
-            Members of all levels welcomed! Monthly meetups on first Wednesdays.
-          </Typography>
-          <br />
-          <iframe title="calendar" src="https://calendar.google.com/calendar/embed?src=hello%40ethmelbourne.co&ctz=Australia%2FMelbourne" style={{ 'border': 0 }} width="800" height="600" frameborder="0" scrolling="no"></iframe>
-          <br />
-          <Link href="https://twitter.com/ethmelbourne">twitter</Link>
-          &nbsp;&nbsp;
-          <Link href="https://lu.ma/u/usr-VEFqG8hvMFtTjmx/">events</Link>
-          &nbsp;&nbsp;
-          <Link href="https://t.me/+Yn85Nzth0XRlM2Jh">telegram</Link>
-          &nbsp;&nbsp;
-          <Link href="https://discord.gg/tDqFSFxSnt">discord</Link>
-          <br />
-          <br />
-          <Link href="https://forms.gle/z4dZDY16ED5m2SrF8">interested in speaking?</Link>
-          <br />
-          <br />
-          email: hello[at]ethmelbourne.co
-        </Grid>
+    <Container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      style={{ marginTop: '50px', textAlign: "center" }}>
+
+      <Grid item md={12}>
+        <img src={logo} alt="logo" width={128} height={128} />
+        <Typography variant="h4">Ethereum Melbourne</Typography>
+        <Typography variant="subtitle1">
+          Members of all levels welcomed! Monthly meetups on first Wednesdays.
+        </Typography>
       </Grid>
-    </>
+
+
+
+      <Grid item lg={12} sx={{ display: { xs: 'none', sm: 'block' } }}>
+        <iframe title="calendar" src="https://calendar.google.com/calendar/embed?src=hello%40ethmelbourne.co&ctz=Australia%2FMelbourne" style={{ 'border': 0 }} width="800" height="600" frameborder="0" scrolling="no"></iframe>
+      </Grid>
+
+      <Grid item xs={12} sx={{ display: { xs: 'block', sm: 'none' } }}>
+        <iframe title="calendar" src="https://calendar.google.com/calendar/embed?height=400&wkst=1&bgcolor=%23ffffff&ctz=Australia%2FMelbourne&mode=AGENDA&src=aGVsbG9AZXRobWVsYm91cm5lLmNv&color=%23039BE5" style={{ 'border': 'solid 1px #777' }} width="350" height="400" frameborder="0" scrolling="no"></iframe>
+      </Grid>
+
+
+      <Grid item md={12}>
+        <Link href="https://twitter.com/ethmelbourne">twitter</Link>
+        &nbsp;&nbsp;
+        <Link href="https://lu.ma/u/usr-VEFqG8hvMFtTjmx/">events</Link>
+        &nbsp;&nbsp;
+        <Link href="https://t.me/+Yn85Nzth0XRlM2Jh">telegram</Link>
+        &nbsp;&nbsp;
+        <Link href="https://discord.gg/tDqFSFxSnt">discord</Link>
+        <br />
+        <br />
+        <Link href="https://forms.gle/z4dZDY16ED5m2SrF8">interested in speaking?</Link>
+        <br />
+        <br />
+        email: hello[at]ethmelbourne.co
+      </Grid>
+    </Container>
   );
 }
 
